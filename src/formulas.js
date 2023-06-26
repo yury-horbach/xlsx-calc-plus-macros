8fa446c38a1cd867c3034d62edabee87262252f0
Yury Horbach <yury@tpsgate.com>
2023-06-26 15:09:20 +0300

    ExcelDateToJSDate parsing

--- src/formulas.js
+++ src/formulas.js
@@ -790,7 +790,30 @@ function time(hours, minutes, seconds) {
     return ((hours * 60 + minutes) * 60 + seconds) * 1000 / MS_PER_DAY;
 }
 
+function ExcelDateToJSDate(serial) {
+    if (serial.getDate) {
+        return serial;
+    }
+    let utc_days  = Math.floor(serial - 25569);
+    let utc_value = utc_days * 86400;
+    let date_info = new Date(utc_value * 1000);
+
+    let fractional_day = serial - Math.floor(serial) + 0.0000001;
+
+    let total_seconds = Math.floor(86400 * fractional_day);
+
+    let seconds = total_seconds % 60;
+
+    total_seconds -= seconds;
+
+    let hours = Math.floor(total_seconds / (60 * 60));
+    let minutes = Math.floor(total_seconds / 60) % 60;
+
+    return new Date(date_info.getFullYear(), date_info.getMonth(), date_info.getDate(), hours, minutes, seconds);
+}
+
 function day(date) {
+    date = ExcelDateToJSDate(date);
     if (!date.getDate) {
         throw Error('#VALUE!');
     }
@@ -802,6 +825,7 @@ function day(date) {
 }
 
 function month(date) {
+    date = ExcelDateToJSDate(date);
     if (!date.getMonth) {
         throw Error('#VALUE!');
     }
@@ -813,6 +837,7 @@ function month(date) {
 }
 
 function year(date) {
+    date = ExcelDateToJSDate(date);
     if (!date.getFullYear) {
         throw Error('#VALUE!');
     }
